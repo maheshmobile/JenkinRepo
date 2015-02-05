4489548f562080cea2fd5c9360e5fffa3eef358f
AFLDLF <Accel@AFLDLFs-Mac-mini.local>
2015-02-05 16:43:44 +0530

    initial commit

---
+++ CKey/KeyboardViewController.h
@@ -0,0 +1,13 @@
+//
+//  KeyboardViewController.h
+//  CKey
+//
+//  Created by AFLDLF on 03/02/15.
+//  Copyright (c) 2015 AFLDLF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KeyboardViewController : UIInputViewController
+
+@end
--- Oops/ViewController.m
+++ Oops/ViewController.m
@@ -16,9 +16,37 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self doCalculation];
+    //[self superClassMethod];
     // Do any additional setup after loading the view, typically from a nib.
 }
+- (void)doCalculation
+{
+    //you can use any string instead "com.mycompany.myqueue"
+    dispatch_queue_t backgroundQueue = dispatch_queue_create("com.mycompany.myqueue", 0);
+    
+    dispatch_async(backgroundQueue, ^{
+        int result = 5656;
 
+        dispatch_async(dispatch_get_main_queue(), ^{
+            NSLog(@"dispatch_get_main_queue");
+            [self updateMyUIWithResult:result];
+        });
+    });
+}
+- (void) updateMyUIWithResult:(int)r{
+    NSLog(@"updateMyUIWithResult");
+
+    self.labelResult.text = [NSString stringWithFormat:@"%d",r];
+    //[self performSelectorOnMainThread:@selector(updateLabel:) withObject:nil waitUntilDone:NO];
+}
+- (void) updateLabel{
+}
+
+- (NSString*) superClassMethod{
+    //NSLog(@"superClassMethod");
+    return @"superClassMethod";
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
